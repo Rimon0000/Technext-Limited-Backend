13a10f165a35b77e229f4c38f5a59d3f996a9f50
Your Name <rimonron.ad@gmail.com>
2024-02-12 13:03:52 +0600

    up to date

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
-import express, { Application, Request, Response } from "express"
-import cors from 'cors'
-import { userRoutes } from "./app/modules/user/user.route"
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { userRoutes } from './app/modules/user/user.route';
 
-
-const app: Application = express()
+const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use("/https://dummyjson.com/users", userRoutes)
-
-
+app.use('/users', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 export default app;
-
